69d3f21374de3cc55a979e013c5210492a0a2729
poornima-moj <poornima.krishnasamy@digital.justice.gov.uk>
2021-07-08 17:10:08 +0100

    Add missing config pkg

---
+++ pkg/config/config.go
@@ -0,0 +1,38 @@
+package config
+
+import (
+	"log"
+
+	"github.com/kelseyhightower/envconfig"
+)
+
+type EnvPipelineConfig struct {
+	StateBucket    string `required:"true"`
+	StateKeyPrefix string `required:"true"`
+	StateLockTable string `required:"true"`
+	StateRegion    string `required:"true"`
+	Cluster        string `required:"true"`
+	RepoPath       string `required:"true"`
+	NumRoutines    int    `default:"2"`
+}
+
+// NewEnvPipelineConfig sets values for EnvPipelineConfig by fetching it from environment variable
+// and returns a pointer to a the config
+func NewEnvPipelineConfig() *EnvPipelineConfig {
+	var env EnvPipelineConfig
+	err := envconfig.Process("pipeline", &env)
+	if err != nil {
+		log.Fatal(err.Error())
+	}
+	return &env
+}
+
+func NewEnvPipelineTestConfig() *EnvPipelineConfig {
+
+	config := NewEnvPipelineConfig()
+
+	config.Cluster = "test"
+	config.RepoPath = "./"
+	config.StateBucket = "cloud-platform-"
+	return config
+}
